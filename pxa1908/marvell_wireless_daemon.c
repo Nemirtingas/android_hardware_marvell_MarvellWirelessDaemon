8b041d0569e4606b67db486c3fcadd72a0ab92c1
Nemirtingas <nanaki89@hotmail.fr>
2018-12-26 10:10:58 +0100

    Updated licence

--- pxa1908/marvell_wireless_daemon.c
+++ pxa1908/marvell_wireless_daemon.c
@@ -1,6 +1,6 @@
 /*
- * Copyright (C) 2018 The LineageOS Project
- *                     Nemirtingas <nanaki89@hotmail.fr>
+ * Copyright (C) 2016 The CyanogenMod Project
+ *               2017 The LineageOS Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -14,6 +14,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 #define LOG_TAG "marvellWirelessDaemon"
 #include <stdio.h>
 #include <stdlib.h>
